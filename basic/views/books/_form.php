ee46723b8d10e9f99ad19a3e09d2e58b1b9882ba
RomanovV <romanov@osp.ru>
2020-02-09 17:12:49 +0300

    add crud

---
+++ basic/views/books/_form.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Books */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="books-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?php
+        echo $form->field($model, 'author_id')
+        ->dropDownList(
+            \yii\helpers\ArrayHelper::map(\app\models\Authors::find()->asArray()->all(), 'id', 'fullName')
+        )
+    ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
